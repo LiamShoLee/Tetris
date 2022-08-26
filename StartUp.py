20460b7d6a6e19b3d36b8de0913234d56deea069
nickhowe <nick.munro.howe@gmail.com>
2022-08-26 10:38:56 +1000

    Added some paint images I made, some sample button code

--- StartUp.py
+++ StartUp.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+import button
 
 pygame.init()
 
@@ -8,11 +9,23 @@ clock = pygame.time.Clock()
 screen_width = 1070
 screen_height = 720
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Name of Game")
+pygame.display.set_caption("Tetris")
 
-font = pygame.font.Sysfont("comicsansms", 30)
+#font = pygame.font.Sysfont("comicsansms", 30)
+
+#Load in some buttons
+play_img = pygame.image.load('Play_img.png').convert_alpha()
+exit_img = pygame.image.load('Exit_img.png').convert_alpha()
+config_img = pygame.image.load('Configure_img.png').convert_alpha()
+
+play_button = button.Button(100, 200, play_img, 1)
+exit_button = button.Button(450, 200, exit_img, 1)
 
 while True:
+
+    screen.fill((195,195,195))
+    play_button.draw
+    exit_button.draw
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
