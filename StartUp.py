df73a8700ce3b1f60588fff674cb5f72d4d9eb07
nickhowe <nick.munro.howe@gmail.com>
2022-08-26 11:20:36 +1000

    changed button locations to relative

--- StartUp.py
+++ StartUp.py
@@ -18,9 +18,9 @@ play_img = pygame.image.load('Play_img.png')
 exit_img = pygame.image.load('Exit_img.png')
 config_img = pygame.image.load('Configure_img.png')
 
-play_button = button.Button(0, 670, play_img, 1)
-exit_button = button.Button(970, 670, exit_img, 1)
-config_button = button.Button(100, 670, config_img, 1)
+play_button = button.Button(0.15*screen_width, 0.85*screen_height-play_img.get_height()*1, play_img, 1)
+exit_button = button.Button(0.85*screen_width-exit_img.get_width(), 0.85*screen_height-exit_img.get_height()*1, exit_img, 1)
+config_button = button.Button(screen_width/2-config_img.get_width()/2, 0.85*screen_height-config_img.get_height()*1, config_img, 1)
 
 playGame = True
 while playGame:
