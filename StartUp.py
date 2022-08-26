4f807cb8483519273a1670309d96bd54482fa917
nickhowe <nick.munro.howe@gmail.com>
2022-08-26 11:06:34 +1000

    Buttons working

--- StartUp.py
+++ StartUp.py
@@ -14,18 +14,26 @@ pygame.display.set_caption("Tetris")
 #font = pygame.font.Sysfont("comicsansms", 30)
 
 #Load in some buttons
-play_img = pygame.image.load('Play_img.png').convert_alpha()
-exit_img = pygame.image.load('Exit_img.png').convert_alpha()
-config_img = pygame.image.load('Configure_img.png').convert_alpha()
+play_img = pygame.image.load('Play_img.png')
+exit_img = pygame.image.load('Exit_img.png')
+config_img = pygame.image.load('Configure_img.png')
 
-play_button = button.Button(100, 200, play_img, 1)
-exit_button = button.Button(450, 200, exit_img, 1)
+play_button = button.Button(0, 670, play_img, 1)
+exit_button = button.Button(970, 670, exit_img, 1)
+config_button = button.Button(100, 670, config_img, 1)
 
-while True:
+playGame = True
+while playGame:
 
     screen.fill((195,195,195))
-    play_button.draw
-    exit_button.draw
+    if play_button.draw(screen):
+        print("start game")
+    if exit_button.draw(screen):
+        playGame=False
+    if config_button.draw(screen):
+        print("Configure")
+    
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
