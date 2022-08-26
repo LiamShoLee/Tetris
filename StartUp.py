dfad69917beee2c1e7a9b811efd1dacc759fe7e9
nickhowe <nick.munro.howe@gmail.com>
2022-08-26 14:00:14 +1000

    trying to commit

--- StartUp.py
+++ StartUp.py
@@ -17,42 +17,69 @@ pygame.display.set_caption("Tetris")
 
 #Load in some buttons
 image_scale = 1
-play_img = pygame.image.load('assests/Play_img.png')
-exit_img = pygame.image.load('assests/Exit_img.png')
-config_img = pygame.image.load('assests/Configure_img.png')
-score_img = pygame.image.load('assests/Score_img.png')
-title_img = pygame.image.load('assests/Cursed_title.png')
+play_img = pygame.image.load('assets/Play_img.png')
+exit_img = pygame.image.load('assets/Exit_img.png')
+config_img = pygame.image.load('assets/Configure_img.png')
+score_img = pygame.image.load('assets/Score_img.png')
+title_img = pygame.image.load('assets/Cursed_title.png')
+back_img = pygame.image.load('asssets/Back_img.png')
 
 play_x = .085*screen_width
 config_x = play_x+play_img.get_width()*image_scale+screen_width*.10
-score_x = config_x+config_img.get_width()*image_scale+screen_width*.10
-exit_x = score_x+score_img.get_width()*image_scale+screen_width*.10
+score_x = config_x + config_img.get_width()*image_scale+screen_width*.10
+exit_x = score_x + score_img.get_width()*image_scale+screen_width*.10
+back_x = exit_x - screen_width*.10 - back_img.get_width()*image_scale
 
-play_button = button.Button(play_x, 0.85*screen_height-play_img.get_height()*image_scale, play_img, image_scale)
-config_button = button.Button(config_x, 0.85*screen_height-config_img.get_height()*image_scale, config_img, image_scale)
-score_button = button.Button(score_x, 0.85*screen_height-score_img.get_height()*image_scale, score_img, image_scale)
-exit_button = button.Button(exit_x, 0.85*screen_height-exit_img.get_height()*image_scale, exit_img, image_scale)
+play_button = button.Button(play_x, 0.95*screen_height-play_img.get_height()*image_scale, play_img, image_scale)
+config_button = button.Button(config_x, 0.95*screen_height-config_img.get_height()*image_scale, config_img, image_scale)
+score_button = button.Button(score_x, 0.95*screen_height-score_img.get_height()*image_scale, score_img, image_scale)
+exit_button = button.Button(exit_x, 0.95*screen_height-exit_img.get_height()*image_scale, exit_img, image_scale)
+back_button = button.Button(back_x,0.95*screen_height-back_img.get_height()*image_scale, back_img, image_scale)
 
-playGame = True
-while playGame:
+def MainMenu():
+    RunGame = True
+    while RunGame:
+        screen.fill((195,195,195))
+        if play_button.draw(screen):
+            print("start game")
+        if exit_button.draw(screen):
+                pygame.quit()
+                sys.exit()
+        if config_button.draw(screen):
+            ConfigMenu()
+        if score_button.draw(screen):
+            print("Top Scores")
 
-    screen.fill((195,195,195))
-    if play_button.draw(screen):
-        print("start game")
-    if exit_button.draw(screen):
-        playGame=False
-    if config_button.draw(screen):
-        print("Configure")
-    if score_button.draw(screen):
-        print("Top Scores")
+        screen.blit(title_img,(title_x,title_y))
+        
 
-    screen.blit(title_img,(title_x,title_y))
-    
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+                
+        pygame.display.update()
+        clock.tick(15)
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+
+def ConfigMenu():
+    RunGame = True
+    while RunGame:
+        screen.fill((195,195,195))
+        if exit_button.draw(screen):
             pygame.quit()
             sys.exit()
-            
-    pygame.display.update()
-    clock.tick(15)
+        if back_button.draw(screen):
+            return
+        screen.blit(title_img,(title_x,title_y))
+        
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+                
+        pygame.display.update()
+        clock.tick(15)
+
+MainMenu()
