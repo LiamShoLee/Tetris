f90796d7b3de1220b4548e40b4826510f756709d
nickhowe <nick.munro.howe@gmail.com>
2022-08-26 12:17:44 +1000

    Main menu prototype

--- StartUp.py
+++ StartUp.py
@@ -8,19 +8,30 @@ clock = pygame.time.Clock()
 
 screen_width = 1070
 screen_height = 720
+title_x = (1070-560)/2
+title_y = (0)
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Tetris")
 
 #font = pygame.font.Sysfont("comicsansms", 30)
 
 #Load in some buttons
+image_scale = 1
 play_img = pygame.image.load('Play_img.png')
 exit_img = pygame.image.load('Exit_img.png')
 config_img = pygame.image.load('Configure_img.png')
+score_img = pygame.image.load('Score_img.png')
+title_img = pygame.image.load('Cursed_title.png')
 
-play_button = button.Button(0.15*screen_width, 0.85*screen_height-play_img.get_height()*1, play_img, 1)
-exit_button = button.Button(0.85*screen_width-exit_img.get_width(), 0.85*screen_height-exit_img.get_height()*1, exit_img, 1)
-config_button = button.Button(screen_width/2-config_img.get_width()/2, 0.85*screen_height-config_img.get_height()*1, config_img, 1)
+play_x = .085*screen_width
+config_x = play_x+play_img.get_width()*image_scale+screen_width*.10
+score_x = config_x+config_img.get_width()*image_scale+screen_width*.10
+exit_x = score_x+score_img.get_width()*image_scale+screen_width*.10
+
+play_button = button.Button(play_x, 0.85*screen_height-play_img.get_height()*image_scale, play_img, image_scale)
+config_button = button.Button(config_x, 0.85*screen_height-config_img.get_height()*image_scale, config_img, image_scale)
+score_button = button.Button(score_x, 0.85*screen_height-score_img.get_height()*image_scale, score_img, image_scale)
+exit_button = button.Button(exit_x, 0.85*screen_height-exit_img.get_height()*image_scale, exit_img, image_scale)
 
 playGame = True
 while playGame:
@@ -32,6 +43,10 @@ while playGame:
         playGame=False
     if config_button.draw(screen):
         print("Configure")
+    if score_button.draw(screen):
+        print("Top Scores")
+
+    screen.blit(title_img,(title_x,title_y))
     
 
     for event in pygame.event.get():
