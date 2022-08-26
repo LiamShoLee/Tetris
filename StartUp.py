d1f467c01a2710a8e18e1f008d2e3ea436be9d86
Liam Lee <reeriam@gmail.com>
2022-08-26 13:22:39 +1000

    revert

--- StartUp.py
+++ StartUp.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+import button
 
 pygame.init()
 
@@ -7,8 +8,10 @@ clock = pygame.time.Clock()
 
 screen_width = 1070
 screen_height = 720
+title_x = (1070-560)/2
+title_y = (0)
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Name of Game")
+pygame.display.set_caption("Tetris")
 
 #font = pygame.font.Sysfont("comicsansms", 30)
 
@@ -19,64 +22,37 @@ exit_img = pygame.image.load('Exit_img.png')
 config_img = pygame.image.load('Configure_img.png')
 score_img = pygame.image.load('Score_img.png')
 title_img = pygame.image.load('Cursed_title.png')
-back_img = pygame.image.load('Back_img.png')
 
 play_x = .085*screen_width
 config_x = play_x+play_img.get_width()*image_scale+screen_width*.10
-score_x = config_x + config_img.get_width()*image_scale+screen_width*.10
-exit_x = score_x + score_img.get_width()*image_scale+screen_width*.10
-back_x = exit_x - screen_width*.10 - back_img.get_width()*image_scale
+score_x = config_x+config_img.get_width()*image_scale+screen_width*.10
+exit_x = score_x+score_img.get_width()*image_scale+screen_width*.10
 
-play_button = button.Button(play_x, 0.95*screen_height-play_img.get_height()*image_scale, play_img, image_scale)
-config_button = button.Button(config_x, 0.95*screen_height-config_img.get_height()*image_scale, config_img, image_scale)
-score_button = button.Button(score_x, 0.95*screen_height-score_img.get_height()*image_scale, score_img, image_scale)
-exit_button = button.Button(exit_x, 0.95*screen_height-exit_img.get_height()*image_scale, exit_img, image_scale)
-back_button = button.Button(back_x,0.95*screen_height-back_img.get_height()*image_scale, back_img, image_scale)
+play_button = button.Button(play_x, 0.85*screen_height-play_img.get_height()*image_scale, play_img, image_scale)
+config_button = button.Button(config_x, 0.85*screen_height-config_img.get_height()*image_scale, config_img, image_scale)
+score_button = button.Button(score_x, 0.85*screen_height-score_img.get_height()*image_scale, score_img, image_scale)
+exit_button = button.Button(exit_x, 0.85*screen_height-exit_img.get_height()*image_scale, exit_img, image_scale)
 
-def MainMenu():
-    RunGame = True
-    while RunGame:
-        screen.fill((195,195,195))
-        if play_button.draw(screen):
-            print("start game")
-        if exit_button.draw(screen):
-                pygame.quit()
-                sys.exit()
-        if config_button.draw(screen):
-            ConfigMenu()
-        if score_button.draw(screen):
-            print("Top Scores")
+playGame = True
+while playGame:
 
-        screen.blit(title_img,(title_x,title_y))
-        
+    screen.fill((195,195,195))
+    if play_button.draw(screen):
+        print("start game")
+    if exit_button.draw(screen):
+        playGame=False
+    if config_button.draw(screen):
+        print("Configure")
+    if score_button.draw(screen):
+        print("Top Scores")
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-                
-        pygame.display.update()
-        clock.tick(15)
+    screen.blit(title_img,(title_x,title_y))
+    
 
-
-def ConfigMenu():
-    RunGame = True
-    while RunGame:
-        screen.fill((195,195,195))
-        if exit_button.draw(screen):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        if back_button.draw(screen):
-            return
-        screen.blit(title_img,(title_x,title_y))
-        
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-                
-        pygame.display.update()
-        clock.tick(15)
-
-MainMenu()
+            
+    pygame.display.update()
+    clock.tick(15)
