81dc9e4c070837bd2c9c7f4f8c9b90a69e2ee24c
nickhowe <nick.munro.howe@gmail.com>
2022-09-21 11:24:14 +1000

    changed button to work with MVC

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@
 #fieldSize is (int,int),level is int, extend is bool, 
 
 class GameSettings:
-    def __init__(defaultSize,defaultLevel,defaultExtend,defaultMode):
+    def __init__(self,defaultSize,defaultLevel,defaultExtend,defaultMode):
         self.fieldSize = defaultSize
         self.gameStartLevel = defaultLevel
         self.extended = defaultExtend
