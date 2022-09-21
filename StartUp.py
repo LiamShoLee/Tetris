dd720e320bba8581c550d9acfe7bfb792c2c0e4a
nickhowe <nick.munro.howe@gmail.com>
2022-09-21 12:54:29 +1000

    added button names

--- StartUp.py
+++ StartUp.py
@@ -33,11 +33,11 @@ score_x = config_x + config_img.get_width()*image_scale+screen_width*.10
 exit_x = score_x + score_img.get_width()*image_scale+screen_width*.10
 back_x = exit_x - 100
 
-play_button = button.Button(play_x, (0.95*screen_height-play_img.get_height()*image_scale), play_img, image_scale)
-config_button = button.Button(config_x, 0.95*screen_height-config_img.get_height()*image_scale, config_img, image_scale)
-score_button = button.Button(score_x, 0.95*screen_height-score_img.get_height()*image_scale, score_img, image_scale)
-exit_button = button.Button(exit_x, 0.95*screen_height-exit_img.get_height()*image_scale, exit_img, image_scale)
-back_button = button.Button(back_x,0.95*screen_height-back_img.get_height()*image_scale, back_img, image_scale)
+play_button = button.Button("play",play_x, (0.95*screen_height-play_img.get_height()*image_scale), play_img, image_scale)
+config_button = button.Button("config_button",config_x, 0.95*screen_height-config_img.get_height()*image_scale, config_img, image_scale)
+score_button = button.Button("score_button",score_x, 0.95*screen_height-score_img.get_height()*image_scale, score_img, image_scale)
+exit_button = button.Button("exit_button",exit_x, 0.95*screen_height-exit_img.get_height()*image_scale, exit_img, image_scale)
+back_button = button.Button("back_button",back_x,0.95*screen_height-back_img.get_height()*image_scale, back_img, image_scale)
 
 topsScoreText = pygame.font.Font.render(pygame.font.Font("assets/MarioFont/SuperMario256.ttf", 48),"Top Scores",True,'black',None)
 
@@ -58,18 +58,18 @@ heightText = pygame.font.Font.render(font2,"Height",True,('black'),None)
 widthText = pygame.font.Font.render(font2,"Width",True,('black'),None)
 
 #configure page buttons
-fieldSizePlusButton = button.surfaceButton(705,400,plusText)
-fieldSizeMinusButton = button.surfaceButton(635,400,minusText)
-fieldSizePlusButton2 = button.surfaceButton(965,400,plusText)
-fieldSizeMinusButton2 = button.surfaceButton(890,400,minusText)
-heightButton = button.surfaceButton(520,400,plusText)
-widthButton = button.surfaceButton(720,400,minusText)
-extendedButton = button.surfaceButton(500,500,extendText)
-normalButton = button.surfaceButton(750,500,normalText)
-aiButton = button.surfaceButton(500,200,aiText)
-playerButton = button.surfaceButton(600,200,playerText)
-plusLevelButton = button.surfaceButton(560,300,plusText)
-minusLevelButton = button.surfaceButton(500,300,minusText)
+fieldSizePlusButton = button.surfaceButton("fieldSizePlusButton",705,400,plusText)
+fieldSizeMinusButton = button.surfaceButton("fieldSizeMinusButton",635,400,minusText)
+fieldSizePlusButton2 = button.surfaceButton("fieldSizePlusButton2",965,400,plusText)
+fieldSizeMinusButton2 = button.surfaceButton"fieldSizeMinusButton2",(890,400,minusText)
+heightButton = button.surfaceButton("heightButton",520,400,plusText)
+widthButton = button.surfaceButton("widthButton",720,400,minusText)
+extendedButton = button.surfaceButton("extendedButton",500,500,extendText)
+normalButton = button.surfaceButton("normalButton",750,500,normalText)
+aiButton = button.surfaceButton("aiButton",500,200,aiText)
+playerButton = button.surfaceButton("playerButton",600,200,playerText)
+plusLevelButton = button.surfaceButton("plusLevelButton",560,300,plusText)
+minusLevelButton = button.surfaceButton("minusLevelButton",500,300,minusText)
 
 #Text for start-up
 yearCourseText = pygame.font.Font.render(font2,"2805ICT 2022",True,('black'),None)  #Here
@@ -140,9 +140,6 @@ def ConfigMenu():
     RunGame = True
     while RunGame:
         screen.fill((195,195,195))
-      #  if exit_button.draw(screen):
-      #      pygame.quit()
-      #      sys.exit()
         if back_button.draw(screen):
             return
         screen.blit(gameModeText,(100,200))
@@ -185,5 +182,8 @@ def ConfigMenu():
                 
         pygame.display.update()
         clock.tick(15)
+    
+    def MenuButtonHandler(buttonName):
+
 
 MainMenu()
