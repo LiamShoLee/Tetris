41ee8583a507514a19cc66d95a2159f1f395253f
nickhowe <nick.munro.howe@gmail.com>
2022-09-21 18:01:00 +1000

    modifying commands

---
+++ ButtonCommand.py
@@ -0,0 +1,31 @@
+#Command Class for Tetris
+from abc import ABCMeta, abstractstaticmethod
+
+
+class IButton(metaclass = ABCMeta):
+   
+    @abstractstaticmethod
+    def execute():
+        #the function name for button presses
+
+class MenuInvoker
+
+
+class Play_command 
+class Config_command
+class Score_command
+class Exit_command 
+class Back_command 
+
+class FieldHeightPlusCommand
+class FieldHeightMinusCommand 
+class FieldWidthPlusCommand2
+class FieldWidthMinusCommand2 
+class HeightCommand 
+WidthCommand
+ExtendedCommand
+NormalCommand
+AiCommand
+PlayerCommand
+PlusLevelCommand
+MinusLevelCommand
--- button.py
+++ button.py
@@ -8,20 +8,23 @@ class Button:
 		self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
 		self.rect = self.image.get_rect()
 		self.rect.topleft = (x, y)
+		self.isPressed = False
 
 	def draw(self, surface):
 		surface.blit(self.image, (self.rect.x, self.rect.y))
 
+	def buttonPoller(self):
+		if self.rect.collidepoint(pygame.mouse.get_pos()):
+			if pygame.mouse.get_pressed()[0] == 1 and self.isPressed == False:
+				return True
+		else: return False
 
 class surfaceButton(Button):
-	def __init__(self,x,y,Surface):
+	def __init__(self,name,x,y,Surface):
+		self.name = name
 		self.image = Surface
 		self.rect = self.image.get_rect()
 		self.rect.topleft = (x, y)
 	
 
-def buttonPoller(button,isAlreadyPressed):
-		if button.rect.collidepoint(pygame.mouse.get_pos()):
-			if pygame.mouse.get_pressed()[0] == 1 and isAlreadyPressed == False:
-				return True
-		else: return False
+
