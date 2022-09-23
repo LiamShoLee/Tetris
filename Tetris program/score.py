bbf7217a9e8ab42e8d6dfc93e9acac7fe16d79a7
Liam Lee <reeriam@gmail.com>
2022-09-23 18:41:12 +1000

    score comments

--- Tetris program/score.py
+++ Tetris program/score.py
@@ -1,24 +1,43 @@
 def clear_rows(grid, locked):
-    inc = 0
+    """Clears all completed lines in the game and shifts entire grid down to fill in the eliminated lines.
+        Also returns the score depending on how many lines were eliminated at once
+
+    Parameters:
+        grid (2D array of RGB tuples): data representation of the color of each tile in the grid
+        locked (dict): dictionary of positions where blocks have been locked
+
+    Returns:
+        int: the number of points to be awarded for clearing lines, or 0 if no lines were cleared
+    """
+    lines_cleared = 0
+    
+    #Check each lines starting from bottom of grid
     for i in range(len(grid)-1, -1, -1):
         row = grid[i]
-        if (0,0,0) not in row:
-            inc += 1
-            ind = i
+        
+        if (0,0,0) not in row: #If no empty tiles in the row
+            lines_cleared += 1
+            index = i
+            
+            #Delete line from the grid
             for j in range(len(row)):
                 try:
                     del locked[(j,i)]
                 except:
                     continue
 
-    if inc > 0:
+    #Shifts entire grid down if there are any lines eliminated
+    if lines_cleared > 0:
+        
+        #For each row in the grid
         for key in sorted(list(locked), key=lambda x: x[1])[::-1]:
             x, y = key
-            if y < ind:
-                newKey = (x, y + inc)
+            
+            if y < index: #Shift down current row if it was above the last eliminated line
+                newKey = (x, y + lines_cleared)
                 locked[newKey] = locked.pop(key)
 
-    match inc:
+    match lines_cleared:
         case 1:
             return 100
         case 2:
@@ -28,21 +47,3 @@ def clear_rows(grid, locked):
         case 4:
             return 1000
     return 0
-
-def update_score(nscore):
-    score = max_score()
-
-    with open('scores.txt', 'w') as f:
-        if int(score) > nscore:
-            f.write(str(score))
-        else:
-            f.write(str(nscore))
-
-
-def max_score():
-    with open('scores.txt', 'r') as f:
-        lines = f.readlines()
-        score = lines[0].strip()
-
-    return score
-
