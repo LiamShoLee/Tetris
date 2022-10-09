e32cc99c7a552faeee2ee7d1708f8eac2ef8facb
Liam Lee <reeriam@gmail.com>
2022-10-09 17:30:07 +1000

    sdfsdfsd

--- Tetris program/adriancodethis.py
+++ Tetris program/adriancodethis.py
@@ -3,9 +3,23 @@ scores = [line.strip('\n')
     for line in open('scores.txt', 'r').readlines()]
 
 my_list = []
+
+top_score = 6000
+count = 1
 for score in scores:
     sep = score.split()
-    my_list.append(sep)
+    if int(sep[2]) < top_score:
+        new_score = [count,"New Player", top_score]
+        my_list.append(new_score)
+        count += 1
+        my_list.append([count, sep[1], sep[2]])
+        top_score = -1
+    else:
+        my_list.append([count, sep[1], sep[2]])
+    
+    if count == 10:
+        break
+    count += 1
 
 #For a given score compare against 2nd index of all values. If it finds that given score is larger than a number.
 #Rewrite insert score into that position with name, then rewrite the list.
