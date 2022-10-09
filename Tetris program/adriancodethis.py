887a6984d8ab6f72097e88dfd60f8ad4f1fb3a6a
Liam Lee <reeriam@gmail.com>
2022-10-09 17:21:19 +1000

    sdfsdf

--- Tetris program/adriancodethis.py
+++ Tetris program/adriancodethis.py
@@ -7,5 +7,8 @@ for score in scores:
     sep = score.split()
     my_list.append(sep)
 
+#For a given score compare against 2nd index of all values. If it finds that given score is larger than a number.
+#Rewrite insert score into that position with name, then rewrite the list.
+#Write new list into score.txt
 for thing in my_list:
     print(thing)
